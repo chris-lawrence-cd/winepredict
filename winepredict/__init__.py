136d4dd525772bc4202ed770c6784b93e945394e
Chris Lawrence <156590252+chris-lawrence-cd@users.noreply.github.com>
2024-08-17 21:15:53 +0100

    Update __init__.py

--- winepredict/__init__.py
+++ winepredict/__init__.py
@@ -2,3 +2,4 @@ from .data_processing import download_fred_data, process_fred_data, preprocess_a
 from .model_training import train_and_evaluate_models, tune_and_evaluate_catboost
 from .model_evaluation import evaluate_model, cross_validate_model
 from .visualization import plot_actual_vs_predicted, visualize_residuals
+from .info import info
