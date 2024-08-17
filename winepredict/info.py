8d93773754357426f8ddb0d63877318ee46129a0
Chris Lawrence <156590252+chris-lawrence-cd@users.noreply.github.com>
2024-08-17 21:15:34 +0100

    Add files via upload

---
+++ winepredict/info.py
@@ -0,0 +1,15 @@
+def info():
+    """Prints information about the WinePredict library."""
+    ascii_art = """
+██╗    ██╗██╗███╗   ██╗███████╗    ██████╗ ██████╗ ███████╗██████╗ ██╗ ██████╗████████╗
+██║    ██║██║████╗  ██║██╔════╝    ██╔══██╗██╔══██╗██╔════╝██╔══██╗██║██╔════╝╚══██╔══╝
+██║ █╗ ██║██║██╔██╗ ██║█████╗      ██████╔╝██████╔╝█████╗  ██║  ██║██║██║        ██║   
+██║███╗██║██║██║╚██╗██║██╔══╝      ██╔═══╝ ██╔══██╗██╔══╝  ██║  ██║██║██║        ██║   
+╚███╔███╔╝██║██║ ╚████║███████╗    ██║     ██║  ██║███████╗██████╔╝██║╚██████╗   ██║   
+ ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚══════╝    ╚═╝     ╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝ ╚═════╝   ╚═╝   
+"""
+    print(ascii_art)
+    print("Welcome to the WinePredict library!")
+    print("This library provides tools for predicting wine prices using various machine learning models.")
+    print("It includes functions for data processing, model training, evaluation, and visualization.")
+    print("Use this library to build, train, and evaluate models to forecast wine prices based on economic indicators.")
